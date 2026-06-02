237bf7b58f16c886c81d7b5f6979926250a686c7
Harish-Patill <hbpatil275@gmail.com>
2026-06-02 05:47:49 +0000

    employee details

---
+++ Structures/employee_info.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+struct employee{
+    int id;
+    char name[50];
+    float salary;
+    char email[50];
+};
+
+void print(struct employee *eptr){
+
+    printf("\nuser id: %d\nuser name: %s\nsalary: %.2f\nuser email: %s\n",eptr->id,eptr->name,eptr->salary,eptr->email);
+}
+
+int main(){
+    struct employee e1;
+    printf("Enter user id, user name, salary, and email: ");
+    scanf("%d %s %f %s",&e1.id,e1.name,&e1.salary,e1.email);
+    print(&e1);
+
+}

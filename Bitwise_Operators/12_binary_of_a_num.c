ffd566cfb756757d84393a8c7bc712f9a051bb8f
Harish-Patill <hbpatil275@gmail.com>
2026-06-30 17:38:48 +0530

    bitwise: 12

---
+++ Bitwise_Operators/12_binary_of_a_num.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int num=0;
+    int n=0;
+
+    printf("Enter the number: ");
+    scanf("%d",&num);
+
+    for(int n=31;n>=0;n--){
+        if((num & (1<<n)) == 0){
+            printf("0");
+        }
+        else{
+            printf("1");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}

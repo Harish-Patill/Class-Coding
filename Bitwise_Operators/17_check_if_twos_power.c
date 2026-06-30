834ea1e2ba63665541e5b83d5d53f9de9f7022b3
Harish-Patill <hbpatil275@gmail.com>
2026-06-30 23:34:15 +0530

    Bitwise: 17

---
+++ Bitwise_Operators/17_check_if_twos_power.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int num=0;
+    int count_zero=0;
+    int count_one=0;
+
+    printf("Enter the num: ");
+    scanf("%d",&num);
+
+    for(int n=31;n>=0;n--){
+        if((num & (1<<n)) != 0){
+            count_one++;
+        }
+    }
+
+    if(count_one == 1){
+        printf("%d is 2's Power\n",num);
+    }
+    else{
+        printf("%d is Not 2's Power\n",num);
+    }
+    return 0;
+}
